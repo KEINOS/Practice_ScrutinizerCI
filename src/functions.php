e812f95c9c106b6bc3ecf6f5cb4d60081e0cacf5
KEINOS <github@keinos.com>
2018-12-14 17:05:43 +0900

    fix: PRS-2 (Fix Incorrect spacing)

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@ function parrotry($string)
     return $string;
 }
 
-function getValue($key, array $array, $default=null)
+function getValue($key, array $array, $default = null)
 {
     return isset($array[$key]) ? $array[$key] : $default;
 }
